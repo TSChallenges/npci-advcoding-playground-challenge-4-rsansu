2e60c80040d8ba55a26e0f61ef944032869081f2
rsansu <rsansu@gmail.com>
2025-02-22 14:26:47 +0530

    Update Account.java

--- src/main/java/com/bankmgmt/app/entity/Account.java
+++ src/main/java/com/bankmgmt/app/entity/Account.java
@@ -8,6 +8,7 @@ public class Account {
     private String email;
 
     // Constructors, getters, and setters
+   
 
     public Account(Integer id, String accountHolderName, String accountType, Double balance, String email) {
         this.id = id;
@@ -18,6 +19,46 @@ public class Account {
     }
 
     // TODO: Add getters and setters
+ public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Account() {
+    }
 
 }
